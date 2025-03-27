a59fc02921cbeae64309505705c3559d66474513
Fatahillah Ardhi <fatahillah.ardhi@bibit.id>
2025-03-27 16:23:23 +0700

    Update Formula

--- Formula/ideviceconfig.rb
+++ Formula/ideviceconfig.rb
@@ -1,8 +1,8 @@
 class Ideviceconfig < Formula
   desc "iOS Device Management Tool"
-  homepage "https://github.com/fatahillahardhi/ideviceconfig"
+  homepage "https://github.com/fatahillahardhi/homebrew-ideviceconfig"
   version "1.0.0"
-  url "https://github.com/fatahillahardhi/ideviceconfig/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/fatahillahardhi/homebrew-ideviceconfig/archive/refs/tags/v1.0.0.tar.gz"
   
   depends_on "ruby"
   depends_on "libimobiledevice"
