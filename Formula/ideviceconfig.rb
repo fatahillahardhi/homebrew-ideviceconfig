ee2ef777f17405e4dc3196c0c8ff35c4b86453c3
Fatahillah Ardhi <fatahillah.ardhi@bibit.id>
2025-03-27 16:34:12 +0700

    Update new

--- Formula/ideviceconfig.rb
+++ Formula/ideviceconfig.rb
@@ -5,22 +5,9 @@ class Ideviceconfig < Formula
   url "https://github.com/fatahillahardhi/homebrew-ideviceconfig/archive/refs/tags/v1.0.0.tar.gz"
   
   depends_on "ruby"
-  depends_on "libimobiledevice"
-  depends_on "libplist"
-  depends_on "openssl"
 
-  def install
-    system "gem", "install", "nokogiri"
-    
-    bin.install "ideviceconfig"
+  def install    
+    bin.install "bin/ideviceconfig"
   end
 
-  def post_install
-    ohai "Make sure libimobiledevice is installed correctly"
-    ohai "Use 'brew upgrade libimobiledevice' if needed"
-  end
-
-  test do
-    system "#{bin}/ideviceconfig", "-h"
-  end
 end
