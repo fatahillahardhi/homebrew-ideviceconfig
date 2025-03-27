2b7e27cb3428ff2ba555aa04ff23418ac6ab020e
Fatahillah Ardhi <fatahillah.ardhi@bibit.id>
2025-03-27 16:44:30 +0700

    Update Formula

--- Formula/ideviceconfig.rb
+++ Formula/ideviceconfig.rb
@@ -3,7 +3,6 @@ class Ideviceconfig < Formula
   homepage "https://github.com/fatahillahardhi/homebrew-ideviceconfig"
   version "1.0.1"
   url "https://github.com/fatahillahardhi/homebrew-ideviceconfig/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 :nocheck
   
   depends_on "ruby"
 
